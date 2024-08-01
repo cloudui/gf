2ac35a077f29a861f33b60d534e1296e57d9dd28
Eric Chen <ericchen314@gmail.com>
2024-08-01 11:19:06 -0400

    Move login to auth

---
+++ src/routes/auth/+page.server.js
@@ -0,0 +1,13 @@
+
+// export function load({ cookies }) {
+// 	const visited = cookies.get('visited', { path: '/' });
+
+// 	return {
+// 		visited: visited === 'true'
+// 	};
+// }
+
+// export function post({ cookies }) {
+//   cookies.set('visited', 'true', {
+
+// })};
