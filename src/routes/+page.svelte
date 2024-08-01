c866c7fa2c127d27ac221430fbdbd3c0ce89fb56
Eric Chen <ericchen314@gmail.com>
2024-07-31 20:10:19 -0400

    Initial commit with tailwind and daisy

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		Hello
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		/* flex: 0.6; */
+	}
+
+	h1 {
+		width: 100%;
+	}
+	
+</style>
