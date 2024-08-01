15e108bddcc2ab428a3506712ec417adfc09e6ce
Eric Chen <ericchen314@gmail.com>
2024-08-01 17:56:32 -0400

    Add timeline component

---
+++ src/lib/components/Timeline.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  type Event = {
+    date: string;
+    title: string;
+  };
+
+  export let events: Event[];
+</script>
+
+<ul class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+  {#each events as event, index}
+  <li>
+    <div class="timeline-middle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="pink"
+        class="h-5 w-5">
+        <path
+          fill-rule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+          clip-rule="evenodd" />
+      </svg>
+    </div>
+    <div 
+      class="mb-10"
+      class:md:text-end={index % 2 === 0}
+      class:timeline-start={index % 2 === 0} 
+      class:timeline-end={index % 2 !== 0} 
+    >      
+      <time class="font-mono">{event.date}</time>
+      <div class="font-bold">{event.title}</div>
+      </div>
+    <hr />
+  </li>
+  {/each}
+</ul>
+
+<style>
+</style>
